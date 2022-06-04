32d396faacc5ff161d33674870b3b37412c52cfd
Kossi D. T. Saka <kdtsaka@gmail.com>
2022-06-04 10:34:19 +0200

    Fix ACF bug with image and file fields in repeatable fields - https://github.com/humanmade/network-media-library/pull/64/commits/7608ce6a19b951f566edec31e1ece9da7bed5a24

--- network-media-library.php
+++ network-media-library.php
@@ -417,7 +417,7 @@ class ACF_Value_Filter {
 	 *
 	 * @var mixed Field value.
 	 */
-	protected $value = null;
+	protected $value = array();
 
 	/**
 	 * Sets up the necessary action and filter callbacks.
@@ -460,7 +460,7 @@ class ACF_Value_Filter {
 			restore_current_blog();
 		}
 
-		$this->value = $image;
+		$this->value[ $field['name'] ] = $image;
 
 		return $image;
 	}
@@ -474,7 +474,7 @@ class ACF_Value_Filter {
 	 * @return mixed The updated value.
 	 */
 	public function filter_acf_attachment_format_value( $value, $post_id, array $field ) {
-		return $this->value;
+		return $this->value[ $field['name'] ];
 	}
 }
 
